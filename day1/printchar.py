c35142ad027daa323a14a7d8b0857c4d01065ada
djx <scuecdjx@gmail.com>
2018-09-20 09:24:20 +0800

    字符输出

---
+++ day1/printchar.py
@@ -0,0 +1,17 @@
+print('I\'m ok')
+print('I\'m learning\nPython.')
+print('\\\n\\')
+print(r'\\\t\\')
+print('''lin1
+lin2 
+lin3''')
+
+# -*- coding:utf-8 -*-
+n=123
+f=456.789
+s1='hello world'
+s2='hello, \'Adam\''
+s3=r'hello."BART"'
+s4=r'''hello,
+Lisa!'''
+print(n,f,s1,s2,s3,s4)
