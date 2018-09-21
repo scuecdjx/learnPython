2cefc04281a9b660193846b221611ca32fc6ae18
djx <scuecdjx@gmail.com>
2018-09-21 09:38:15 +0800

    编码练习

---
+++ day2/str_code.py
@@ -0,0 +1,14 @@
+#-*- coding: utf-8 -*-
+print(ord('A'))#获取字符的整数表示
+#ord('中')
+print(chr(66))#把编码转化为对应的字符
+#print(chr(25464))
+#'\u4e2d\u6587'
+#'ABC'.encode('ascii')
+#b'ABC'.decode('ascii')
+#len('abc')
+print('hi,%s,goodmorning!'%'James')
+s1=72.0
+s2=85.0
+r=(s2-s1)/s1*100
+print('it\'s up %.2f%%'%r)
