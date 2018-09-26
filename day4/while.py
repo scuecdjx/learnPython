9bdc0798260f34511626c5a4b5b674fcd4751fef
djx <scuecdjx@gmail.com>
2018-09-26 08:47:59 +0800

    循环

---
+++ day4/while.py
@@ -0,0 +1,17 @@
+names=['michael','bob','tracy']
+for name in names:
+	print(name)
+sum= 0
+for x in range(101):
+	sum=sum+x
+print(sum)
+n=1
+sum=0
+while n<100:
+	sum=sum+n
+	n=n+2
+print(sum)
+
+L=['Bart','Lisa','Adam']
+for name in L:
+	print('Hello, %s'%name)
